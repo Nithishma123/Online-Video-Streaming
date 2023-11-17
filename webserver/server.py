e687af9074a98bb1ac8d46050d636a50cbeba3cb
Nithishma123 <nithishmaallu1999@gmail.com>
2023-11-17 14:50:34 -0500

    Updated project

--- webserver/server.py
+++ webserver/server.py
@@ -1,4 +1,3 @@
-
 """
 Columbia's COMS W4111.001 Introduction to Databases
 Example Webserver
@@ -11,13 +10,13 @@ Read about it online.
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response, abort
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=tmpl_dir)
+from flask import Flask, request, render_template, g, redirect, Response, abort, jsonify
+import logging
 
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir, static_url_path='/static')
 
 DATABASEURI = "postgresql://na3062:732244@34.74.171.121/proj1part2"
-
 
 #
 # This line creates a database engine that knows how to connect to the URI above.
@@ -25,36 +24,36 @@ DATABASEURI = "postgresql://na3062:732244@34.74.171.121/proj1part2"
 engine = create_engine(DATABASEURI, future=True)
 
 conn = engine.connect()
-conn.execute(text("""CREATE TABLE IF NOT EXISTS test (id serial,name text);"""))
-conn.execute(text("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');"""))
-conn.commit()
+
 
 @app.before_request
 def before_request():
-  """
+    """
   This function is run at the beginning of every web request
   (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request.
 
   The variable g is globally accessible.
   """
-  try:
-    g.conn = engine.connect()
-  except:
-    print("uh oh, problem connecting to database")
-    import traceback; traceback.print_exc()
-    g.conn = None
+    try:
+        g.conn = engine.connect()
+    except:
+        print("uh oh, problem connecting to database")
+        import traceback;
+        traceback.print_exc()
+        g.conn = None
+
 
 @app.teardown_request
 def teardown_request(exception):
-  """
+    """
   At the end of the web request, this makes sure to close the database connection.
   If you don't, the database could run out of memory!
   """
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
+    try:
+        g.conn.close()
+    except Exception as e:
+        pass
 
 
 #
@@ -72,114 +71,75 @@ def teardown_request(exception):
 #
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
+    # DEBUG: this is debugging code to see what request looks like
+    print(request.args)
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+    #
+    # example of a database query
+    #
+    cursor = g.conn.execute(text("SELECT name FROM test"))
+    g.conn.commit()
 
-  See its API: https://flask.palletsprojects.com/en/2.0.x/api/?highlight=incoming%20request%20data
+    # 2 ways to get results
 
-  """
+    # Indexing result by column number
+    names = []
+    for result in cursor:
+        names.append(result[0])
 
-  # DEBUG: this is debugging code to see what request looks like
-  print(request.args)
+    # Indexing result by column name
+    names = []
+    results = cursor.mappings().all()
+    for result in results:
+        names.append(result["name"])
+    cursor.close()
+    context = dict(data=names)
 
-
-  #
-  # example of a database query 
-  #
-  cursor = g.conn.execute(text("SELECT name FROM test"))
-  g.conn.commit()
-
-  # 2 ways to get results
-
-  # Indexing result by column number
-  names = []
-  for result in cursor:
-    names.append(result[0])  
-
-  # Indexing result by column name
-  names = []
-  results = cursor.mappings().all()
-  for result in results:
-    names.append(result["name"])
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #
-  #     # creates a <div> tag for each element in data
-  #     # will print:
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
+    return render_template("index.html", **context)
 
 
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at:
-#
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("another.html")
-
-
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add(): 
-  name = request.form['name']
-  params_dict = {"name":name}
-  g.conn.execute(text('INSERT INTO test(name) VALUES (:name)'), params_dict)
-  g.conn.commit()
-  return redirect('/')
-
-
-@app.route('/login')
+@app.route('/api/login', methods=['POST'])
 def login():
-    abort(401)
+    payload = request.get_json()
+    username = payload.get('username')
+    cursor = g.conn.execute(text("select * from user_information where name= :username"), {'username': username})
+    result = cursor.fetchall()
+    cursor.close()
+    if result:
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        return jsonify({'message': 'User not found'}), 400
+
+
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    payload = request.get_json()
+    logging.debug(payload)
+    name = payload.get('name')
+    emailid = payload.get('emailid')
+    phone = payload.get('phone')
+    age = payload.get('age')
+    gender = payload.get('gender')
+
+    g.conn.execute(text("insert into user_information(name,email_id,phone_no,age,gender) values(:name,"
+                        ":emailid, :phone, :age, :gender)"),
+                   {'name': name, 'emailid': emailid, 'phone': phone, 'age': age,
+                    'gender': gender})
+    g.conn.commit()
+    return jsonify({'message': 'Login successful'}), 200
 
 
 if __name__ == "__main__":
-  import click
+    import click
 
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        """
     This function handles command line parameters.
     Run the server using:
 
@@ -191,8 +151,9 @@ if __name__ == "__main__":
 
     """
 
-    HOST, PORT = host, port
-    print("running on %s:%d" % (HOST, PORT))
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
-  run()
+
+    run()
---
+++ webserver/static/index.js
@@ -0,0 +1,75 @@
+        // Show only login form initially
+        document.getElementById('signupForm').style.display = 'none';
+
+        // Toggle between login and signup forms
+        document.getElementById('toggleSignup').addEventListener('click', function () {
+            document.getElementById('loginForm').style.display = 'none';
+            document.getElementById('signupForm').style.display = 'block';
+        });
+
+        document.getElementById('toggleLogin').addEventListener('click', function () {
+            document.getElementById('loginForm').style.display = 'block';
+            document.getElementById('signupForm').style.display = 'none';
+        });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        fetch('http://192.168.1.30:8111/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 200) {
+                window.location.href = '/home.html';
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('signupForm');
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const emailid = document.getElementById('emailid').value;
+        const phone = document.getElementById('phone').value;
+        const age = parseInt(document.getElementById('age').value, 10);
+        const gender = document.getElementById('gender').value;
+        const pass = document.getElementById('pass').value;
+        fetch('http://192.168.1.30:8111/api/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, emailid, phone, age, gender, pass }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 200) {
+                window.location.href = '/home.html';
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+});
